d6d6629779ee9bcb896451a832afea470525fdb2
zuicy99 <k.zuicy@gmail.com>
2023-11-30 11:07:17 +0900

    docs : emotion main contents

---
+++ src/components/Main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Contents } from "../styles/mainstyle";
+
+const Main = () => {
+  return <Contents>중심 컨텐츠</Contents>;
+};
+
+export default Main;
---
+++ src/styles/mainstyle.js
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+
+export const Contents = styled.main`
+  width: 100%;
+  min-height: 500px;
+`;
